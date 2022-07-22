5e9a2ac1cd4c21fd0d2763be1ee56423acbf3626
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2022-07-23 00:36:22 +0600

    Create Operator.gd

---
+++ assets/Operator.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+#_______________
+# Operator
+#_______________
+
+#Division 
+
+var a:float = 10
+var b:float = 20
+var result:float = a / b # only float support in math
+var model: String = "Tahsin Abrar" # only string support
+var model_int:int = 102 # only int support in math
+var mod_a = 10;
+var mod_b = 20;
+var plus_add = mod_a + 19
+
+func _ready():
+	a = 12.4
+	print(a)
